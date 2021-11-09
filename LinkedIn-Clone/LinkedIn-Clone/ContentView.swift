fb328885a151adb9376185707108860fc8d67a10
gringgyy <kulnis33@gmail.com>
2021-11-10 00:44:15 +0700

    Create Project

---
+++ LinkedIn-Clone/LinkedIn-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinkedIn-Clone
+//
+//  Created by Kulnis Chattratitiphan on 10/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkedIn-Clone/LinkedIn-Clone/LinkedIn_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedIn_CloneApp.swift
+//  LinkedIn-Clone
+//
+//  Created by Kulnis Chattratitiphan on 10/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct LinkedIn_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
